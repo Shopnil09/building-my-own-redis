0b797e8f75f7e0a0f02fa304c74b0b182a6511f6
Shopnil09 <shopnil.shahriar@gmail.com>
2025-07-12 16:34:22 -0400

    sending multiple ping commands sent by same conn

--- app/main.py
+++ app/main.py
@@ -6,7 +6,12 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, _ = server_socket.accept() 
-    connection.sendall(b"+PONG\r\n")
+    while True: 
+        msg = connection.recv(512)
+        data = msg.decode()
+        
+        if "ping" in data: 
+            connection.send("+PONG\n\r\n".encode())
 
 
 if __name__ == "__main__":
